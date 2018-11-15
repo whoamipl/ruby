0efb5ad98886d74726f45385d994ec2179ad58ba
= <mfierek@protonmail.ch>
2018-11-15 14:39:54 +0100

    Added simple program with one ant

---
+++ Anthill.rb
@@ -0,0 +1,38 @@
+require 'time'
+class AntHill
+    def initialize(x,y,antAmout)
+        @hill = Array.new(x) { Array.new(y)}
+        @antAmout = antAmout
+    end
+
+    def fillAnthill() 
+        (0..@hill.length - 1).each do |row|
+            (0..@hill[0].length - 1).each do |col|
+                @hill[row][col] = '*'
+            end
+        end
+    end
+
+    def printAnthill()
+        cord = getRandomCord()
+        @hill[cord[0]][cord[1]] = 'm'
+        @hill.each { |x|
+            puts x.join(" ")
+        }
+        print("\r")
+        @hill[cord[0]][cord[1]] = '*'
+    end
+
+    def getRandomCord() 
+        r = Random.new
+        return r.rand(0..9), r.rand(0..9)
+    end
+end
+
+a = AntHill.new(10,10,10)
+a.fillAnthill()
+loop do 
+    a.printAnthill()
+    sleep 0.2
+    system 'clear'
+end
