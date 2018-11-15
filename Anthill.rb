47b5d057071db366e08a254c8766b8d335942cb5
= <mfierek@protonmail.ch>
2018-11-15 15:58:24 +0100

    Corrected problem with random cords

--- Anthill.rb
+++ Anthill.rb
@@ -3,28 +3,37 @@ class AntHill
     def initialize(x,y,antAmout)
         @hill = Array.new(x) { Array.new(y)}
         @antAmout = antAmout
+        @x = x
+        @y = y
     end
 
     def fillAnthill() 
         (0..@hill.length - 1).each do |row|
             (0..@hill[0].length - 1).each do |col|
-                @hill[row][col] = ' '
+                @hill[row][col] = '*'
             end
         end
     end
 
     def printAnthill()
-        cord = getRandomCord()
-        @hill[cord[0]][cord[1]] = 'm'
+        oldCords = {}
+        (0..@antAmout).each do |i|
+            cord = getRandomCord()
+            oldCords[i] = cord
+            @hill[cord[0]][cord[1]] = "m"
+        end
         @hill.each { |x|
             puts x.join(" ")
         }
+        oldCords.each do |cord|
+            cord = getRandomCord()
+            @hill[cord[0]][cord[1]] = " "
+        end
         print("\r")
-        @hill[cord[0]][cord[1]] = '*'
     end
 
     def getRandomCord() 
         r = Random.new
-        return r.rand(0..9), r.rand(0..9)
+        return r.rand(0..@x - 1), r.rand(0..@y -1)
     end
 end
